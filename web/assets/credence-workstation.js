c019570024ea19c4a5058b78b2ed4a0e9097f90d
Arthur Davis <arthur@highphidelity.com>
2026-08-22 22:05:21 -0700

    feat(ops): release v2.10.1 - codify v2.10.0 release learnings into progressive skills

--- web/assets/credence-workstation.js
+++ web/assets/credence-workstation.js
@@ -1,4 +1,4 @@
-export const CREDENCE_VERSION = "v2.10.0";
+export const CREDENCE_VERSION = "v2.10.1";
 /**
  * Credence Workstation Engine & Shared Zero-Build Controller (credence-workstation.js)
  * 
