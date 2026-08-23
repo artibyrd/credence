bdf1c0a6f64737be8079cdec0ccec32eabeb3a73
Arthur Davis <arthur@highphidelity.com>
2026-08-22 19:59:40 -0700

    chore(ops): bump ecosystem version to v2.9.0

--- web/assets/credence-workstation.js
+++ web/assets/credence-workstation.js
@@ -1,4 +1,4 @@
-export const CREDENCE_VERSION = "v2.8.1";
+export const CREDENCE_VERSION = "v2.9.0";
 /**
  * Credence Workstation Engine & Shared Zero-Build Controller (credence-workstation.js)
  * 
